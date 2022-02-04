2cd55c3c681f1684c8b7435af85f40a4116f2c89
Abhay mayani <abhaymayani123@gmail.com>
2022-02-04 21:53:00 +0530

    this commit for update and delete

--- nav.php
+++ nav.php
@@ -1,6 +1,8 @@
 <?php 
   //connect to the database
-
+  $insert = false;
+  $update = false;
+  $delete = false;
   $conn = mysqli_connect("localhost","root","","notes");
 
   if($conn){
@@ -10,8 +12,38 @@
     die("Sorry We Fail To connect the database : ".mysqli_connect_error());
   }
 
+  if(isset($_GET['delete'])){
+    $sno = $_GET['delete'];
+    // echo $sno;
+
+    $sql = "DELETE from `notes` where `sno` = $sno";
+    $result = mysqli_query($conn,$sql);
+
+    if($result){
+      $delete = true;
+    }else{
+      echo "The Record Was not Deleted Successfully because this error ". mysqli_error();
+    }
+  }
+
   // echo $_SERVER['REQUEST_METHOD'];
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_POST['snoEdit'])){
+      $sno = $_POST["snoEdit"];
+      $title = $_POST["titleEdit"];
+      $description = $_POST["descriptionEdit"];
+
+      $sql2 = "UPDATE `notes` SET `title` = '$title' , `description` = '$description' WHERE `notes`.`sno` = $sno";
+      $result = mysqli_query($conn,$sql2);
+
+      if($result){  
+        // echo "Updated SuccessFully";
+        $update = true;
+      }else{
+        echo "The record was not inserted successfully becouse of this error". mysqli_error();
+      }
+    }
+    else{
       $title = $_POST["title"];
       $description = $_POST["description"];
 
@@ -20,9 +52,11 @@
 
       if($result){
         // echo "The record has been inserted successfully..!";
+        $insert = true;
       }else{
         echo "The record was not inserted successfully because of this error ". mysqli_error();
       }
+    }
 
   }
 
@@ -40,13 +74,66 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+  <link rel="stylesheet" href="//cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" class="css">
+
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+    crossorigin="anonymous"></script>
+
+  <script src="//cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      $('#myTable').DataTable();
+    });
+  </script>
+
   <title>Crud</title>
 </head>
 
 <body>
+
+  <!-- Button Edit modal -->
+  <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editModal">
+  Launch demo modal
+</button> -->
+
+  <!--Edit Modal -->
+  <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editeModalLabel">Note title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="nav.php/" method="post">
+          <div class="modal-body">
+
+            <input type="hidden" name="snoEdit" id="snoEdit">
+            <div class="mb-3">
+              <label for="title" class="form-label">Note Title</label>
+              <input type="text" class="form-control" id="titleEdit" name="titleEdit" placeholder="Title">
+            </div>
+            <div class="mb-3">
+              <label for="description" class="form-label">Note Description</label>
+              <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div>
+            <!-- <div class="mb-4">
+              <button type="submit" class="btn btn-primary">Edit Note</button>
+            </div> -->
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">PHP Crud</a>
+      <a class="navbar-brand" href="#"><img src="/abhay crud/logo.png" alt="" height = "30px"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -88,6 +175,30 @@
     </div>
   </nav>
 
+  <?php
+    if($insert){
+      echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+      <strong>Success!</strong> Your note has been inserted successfully.
+      <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+
+    }
+    if($update){
+      echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+      <strong>Success!</strong> Your note has been Edited successfully.
+      <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+
+    }
+
+    if($delete){
+      echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+      <strong>Success!</strong> Your note has been deleted successfully.
+      <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+
+    }
+  ?>
 
   <div class="container my-3">
 
@@ -106,13 +217,13 @@
         <button type="submit" class="btn btn-primary" id="btn_submit">Add Note</button>
 
       </div>
-    </form> 
+    </form>
 
   </div>
 
-  <div class="container">
+  <div class="container my-4">
 
-    <table class="table">
+    <table class="table" id="myTable">
       <thead>
         <tr>
           <th scope="col">Sr No.</th>
@@ -123,29 +234,29 @@
       </thead>
       <tbody>
 
-      <?php  
+        <?php  
   
         $sql = "SELECT * FROM `notes`";
         $result = mysqli_query($conn,$sql);
-
+        $sno = 0;
         while($row = mysqli_fetch_assoc($result)){
-
-        echo "<tr>
-        <th scope='row'>".$row['sno']."</th>
-        <td>".$row['title']."</td>
-        <td>".$row['description']."</td>
-        <td>Actions</td>
-      </tr>";
+          $sno = $sno + 1;
+          echo "<tr>
+            <th scope='row'>".$sno."</th>
+            <td>".$row['title']."</td>
+            <td>".$row['description']."</td>
+            <td><button class='edit btn btn-sm btn-primary' id=".$row['sno'].">Edit</button> <button class='delete btn btn-sm btn-danger' id=d".$row['sno'].">Delete</button></td>
+          </tr>";
         
-    }
+      }
   
   ?>
-    
+
       </tbody>
     </table>
 
   </div>
-
+  <hr>
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -158,6 +269,55 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
+
+
+  <script>
+    edits = document.getElementsByClassName('edit');
+    Array.from(edits).forEach((element) => {
+      element.addEventListener("click", (e) => {
+
+        console.log("edit", e.target.parentNode.parentNode);
+
+        tr = e.target.parentNode.parentNode;
+        title = tr.getElementsByTagName("td")[0].innerText;
+        description = tr.getElementsByTagName("td")[1].innerText;
+
+        console.log(title, description);
+
+        titleEdit.value = title;
+        descriptionEdit.value = description;
+        snoEdit.value = e.target.id;  //main error point in update
+        console.log("sno", e.target.id);
+
+        // editModal.toggle()
+        $('#editModal').modal('toggle')
+      })
+    })
+
+    deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+      element.addEventListener("click", (e) => {
+
+        // console.log("delete",e.target.parentNode.parentNode);
+
+        // tr = e.target.parentNode.parentNode;
+        // title = tr.getElementsByTagName("td")[0].innerText;
+        // description = tr.getElementsByTagName("td")[1].innerText;
+
+        sno = e.target.id.substr(1,);
+        console.log("sno = ", sno);    //and main error ponit in delete id noti aavti becouse click ni id class ma lakhi nakhi hti
+
+        if (confirm("Are You Want To Delete ?")) {
+          console.log("yes");
+          window.location = `nav.php?delete=${sno}`;
+        } else {
+          console.log("No");
+        }
+
+      })
+    })
+
+  </script>
 </body>
 
 </html>
